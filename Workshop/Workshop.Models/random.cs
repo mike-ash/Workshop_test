a16458588c0afb26d086f5c4e48de849a4edb69b
Firas <firas.wira@gmail.com>
2016-11-09 15:10:50 +0100

    Added class random

---
+++ Workshop/Workshop.Models/random.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.Models
+{
+    class random
+    {
+        public random()
+        {
+            var ran = 0;
+        }
+    }
+}
